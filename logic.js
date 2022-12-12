f18cc5a20ce7d2ab1b6adc747e64884734c6df2d
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2022-12-12 16:49:20 +0100

    Second Commit

--- logic.js
+++ logic.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+const ccNumInput = document.querySelector(".cc_inputs");
+const submitBtn = document.querySelector(".submit_btn");
+const companyLogo = document.querySelector(".logo");
+const resultDisplay = document.querySelector(".result");
+
+ccNumInput.addEventListener("input", () => {});
