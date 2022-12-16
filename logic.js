3921bf59775a662d27ca7b63080bab47eb6eac2d
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2022-12-16 13:06:00 +0100

    Commit Update

--- logic.js
+++ logic.js
@@ -5,41 +5,40 @@ const submitBtn = document.querySelector(".submit_btn");
 const companyLogo = document.querySelector(".logo");
 const resultDisplay = document.querySelector(".result");
 
-ccNumInput.addEventListener("input", () => {
-  if (ccNumInput.value.length >= 14 && Math.abs(ccNumInput.value)) {
-    handleClick();
-  }
-});
+ccNumInput.addEventListener("input", () => {});
 
 const handleClick = () => {
-  const ccNum = ccNumInput.value.split("").reverse();
+  if (ccNumInput.value.length >= 14 && Math.abs(ccNumInput.value)) {
+    const ccNum = ccNumInput.value.split("").reverse();
 
-  const calcDigits = [];
-  for (let i = 1; i < ccNum.length; i += 2) {
-    calcDigits.push(`${ccNum[i] * 2 > 9 ? ccNum[i] * 2 - 9 : ccNum[i] * 2}`);
-  }
-  for (let j = 0; j < ccNum.length; j += 2) {
-    calcDigits.push(ccNum[j]);
-  }
-
-  const addNums = calcDigits.reduce((num1, num2) => {
-    return Number(num1) + Number(num2);
-  });
-
-  if (addNums % 10 === 0) {
-    resultDisplay.innerText = "✅ VALID";
-    if (ccNumInput.value[0] == 3) {
-      companyLogo.src = "img/Amex Logo.png";
-    } else if (ccNumInput.value[0] == 4) {
-      companyLogo.src = "img/Visa Logo.png";
-    } else if (ccNumInput.value[0] == 5) {
-      companyLogo.src = "img/Mastercard Logo.png";
-    } else if (ccNumInput.value[0] == 6) {
-      companyLogo.src = "img/Discover Logo.png";
+    const calcDigits = [];
+    for (let i = 1; i < ccNum.length; i += 2) {
+      calcDigits.push(`${ccNum[i] * 2 > 9 ? ccNum[i] * 2 - 9 : ccNum[i] * 2}`);
     }
-  } else {
-    resultDisplay.innerText = "❌ INVALID";
+    for (let j = 0; j < ccNum.length; j += 2) {
+      calcDigits.push(ccNum[j]);
+    }
+
+    const addNums = calcDigits.reduce((num1, num2) => {
+      return Number(num1) + Number(num2);
+    });
+
+    if (addNums % 10 === 0) {
+      resultDisplay.innerText = "✅ VALID";
+      if (ccNumInput.value[0] == 3) {
+        companyLogo.src = "img/Amex Logo.png";
+      } else if (ccNumInput.value[0] == 4) {
+        companyLogo.src = "img/Visa Logo.png";
+      } else if (ccNumInput.value[0] == 5) {
+        companyLogo.src = "img/Mastercard Logo.png";
+      } else if (ccNumInput.value[0] == 6) {
+        companyLogo.src = "img/Discover Logo.png";
+      }
+    } else {
+      resultDisplay.innerText = "❌ INVALID";
+    }
   }
 };
 
 submitBtn.addEventListener("click", handleClick);
+handleClick();
